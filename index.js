ef2c36ad79824bbe3aa83b345718ade3e7d5e1fe
ManasiDeshmukh <deshmukhmanasi9@gmail.com>
2022-08-31 15:21:18 +0530

    Microservice and Kafka integration

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use('/api/admin',require("./routes/adminRoutes"))
 //app.use('/api/medicine',require("./routes/medicineRoutes"))
 app.use('/api/medicine',require("./routes/routes.productDbAdmin"))
 
+app.use('/api/reorder',require("./routes/reorderRoutes"))
 const {errorHandler}=require("./middleware/error")
 
 app.use('/api/users',require("./routes/userRoutes"))
