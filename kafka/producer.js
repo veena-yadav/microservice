e5a0c96e190323c018b0ca29f8b2de380bb54716
ManasiDeshmukh <51395889+ManasiDeshmukh@users.noreply.github.com>
2022-09-10 21:46:39 +0530

    Update producer.js

--- kafka/producer.js
+++ kafka/producer.js
@@ -39,17 +39,14 @@ module.exports = function (message) {
           .createTransport({
             service: "gmail",
             auth: {
-<<<<<<< HEAD
+
             // user: "telstrakafkanetworking2@gmail.com",
              // pass: "yznqnswnzohcdisw",
              user: process.env.EID,
             pass: process.env.PASS,
-=======
-             user: "telstrakafkanetworking2@gmail.com",
-              pass: "yznqnswnzohcdisw",
-              //  user: process.env.EID,
-              // pass: process.env.PASS,
->>>>>>> 75babd6305a23181989f0e13c3442546d19f2dbc
+
+           
+
               port: 465,
               host: "smtp.gmail.com",
             },
