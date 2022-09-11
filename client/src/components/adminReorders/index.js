b54fac892a923aa0f0ae39457343f9fff77f2db9
Sayush Kamat <sayush.kamat@rediffmail.com>
2022-09-11 15:24:47 +0530

    Fixed Accept Now Button
    Fixed Accept Now

--- client/src/components/adminReorders/index.js
+++ client/src/components/adminReorders/index.js
@@ -7,40 +7,35 @@ import IconButton from "@mui/material/IconButton";
 import axios from "axios";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import CancelIcon from "@mui/icons-material/Cancel";
-
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { TextField } from "@mui/material";
 // import Box from '@mui/material/Box';
 // import Button from '@mui/material/Button';
 // import Typography from '@mui/material/Typography';
 // import Fab from '@mui/material/Fab';
 // import AddIcon from '@mui/icons-material/Add';
-// const style = {
-//   position: 'absolute',
-//   top: '50%',
-//   left: '50%',
-//   transform: 'translate(-50%, -50%)',
-//   width: 400,
-//   bgcolor: 'background.paper',
-//   border: '2px solid #000',
-//   boxShadow: 24,
-//   p: 4,
-// };
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 const AdminReorders = () => {
   const [medicines, setMedicines] = useState([]);
-
-  //   const  setCart=(sm,smq,smp)=>{
-
-  //     // console.log(`${selectedMedicineQuantity} in save button `)
-  //     const newItem={
-  //       itemName:sm,
-  //       quantity:selectedMedicineQuantity,
-  //       price:smp
-  //     }
-  //     const newArr=medicinesCart.set(selectedMedicine,newItem)
-  //     setMedicineCart(newArr)
-  //    setOpen(false)
-
-  //   }
-
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+const [modalMedicine,setModalMedicine]=useState('')
+const [modalMedicineQuantity,setModalMedicineQuantity]=useState(0)
+const [medicineObjModal,setMedicineObjModal]=useState({})
   useEffect(() => {
     getMedicine();
   }, []);
@@ -67,22 +62,54 @@ const AdminReorders = () => {
     getMedicine();
   };
   // accept reorders admin side
-  const AcceptMedicine = async (medicineObject) => {
-    const { price, itemName, quantity } = medicineObject;
+  const AcceptMedicine = async (medicineObject,newQuantity) => {
+    const { price, itemName ,minimumThresholdValue } = medicineObject;
     console.log("accepted med");
+    if(newQuantity<minimumThresholdValue)
+    {
+      handleClose()
+      alert("Enter Quantity > Minimum Threshold")
+      return
+    }
     try {
       const acceptedMedicine = await axios.patch(
         `http://localhost:5000/api/medicine/update/${medicineObject.itemName}`,
-        { price, quantity, itemName }
+        { price, quantity:newQuantity, itemName ,minimumThresholdValue}
       );
     } catch (err) {
       console.log(err);
     }
+    handleClose()
     getMedicine();
   };
 
   return (
     <div>
+       <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <center>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+           Accept Medicine {modalMedicine}
+          </Typography>
+          <TextField label="Medicine Quantity" value={modalMedicineQuantity} onChange={(e)=>{
+            setModalMedicineQuantity(e.target.value)
+          }}/><br/>
+          <center>
+          <Button  variant="contained" style={{
+            marginTop:"20px"
+          }} color="primary" onClick={()=>{
+            
+            AcceptMedicine(medicineObjModal,modalMedicineQuantity)
+          }}>Accept Now</Button>
+          </center>
+          </center>
+        </Box>
+      </Modal>
       <div className="medicineTableDiv">
         <table className="medicineTable">
           <thead>
@@ -91,6 +118,7 @@ const AdminReorders = () => {
               <th>Price</th>
               <th> Quantity</th>
               <th>Total Price</th>
+              <th>Threshhold</th>
               <th>Accept</th>
               <th>Reject</th>
             </tr>
@@ -103,10 +131,15 @@ const AdminReorders = () => {
                   <td>{med.price} </td>
                   <td> {med.quantity} </td>
                   <td>{med.price * med.quantity}</td>
+                  <td>{med.minimumThresholdValue}</td>
                   <td>
                     <IconButton
                       onClick={() => {
-                        AcceptMedicine(med);
+                        setModalMedicine(med.itemName)
+                        setMedicineObjModal(med)
+                        handleOpen()
+                       
+                        
                       }}
                     >
                       <CheckCircleIcon color="success" />
